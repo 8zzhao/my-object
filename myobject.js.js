8a4d7f06dfc68e4b00a2811d30d530edc6268b14
8zzhao <31480915+8zzhao@users.noreply.github.com>
2017-10-20 09:17:56 -0500

    Add files via upload

---
+++ myobject.js.js
@@ -0,0 +1,18 @@
+function face (n,l,h){
+this.name=n;
+this.height=h;
+this.level=l;
+let score;
+    this.score=function(){
+        score=level*height
+    if (score < 25);{
+        console.log("I will get married when I past 45")}
+    if (score = 25);{
+        console.log("decent")}
+    if (score > 25);{
+        console.log("I am so happy")}
+return score
+    };
+}
+
+face ("robert", 4, 6);
